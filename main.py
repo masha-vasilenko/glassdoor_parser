d00b5860b3c35bf176a7c1f69082cda63bc2013a
mashamasha <mvasilenko@dons.usfca.edu>
2020-03-10 13:48:24 -0700

    created secret json, launched webdriver and logged in

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import selenium
 from selenium import webdriver
 import json
 
+
 def main():
     print(selenium.__version__)
+
 
 def gd_login(driver, email, pwd):
     userfield = driver.find_element_by_id("userEmail")
@@ -17,15 +19,13 @@ driver_path = '/usr/local/bin/chromedriver'
 login_url = 'https://www.glassdoor.com/profile/login_input.htm'
 
 # Loading email and password from secret.json
-secret_data = {}
 with open('secret.json') as f:
-     secret_data = json.load(f)
+    secret_data = json.load(f)
 
 email = secret_data['email']
 pwd = secret_data['pwd']
 
 # Launch web driver
-
 driver = webdriver.Chrome(driver_path)
 driver.get(login_url)
 driver.implicitly_wait(100)
@@ -36,8 +36,6 @@ gd_login(driver, email, pwd)
 
 # Quit the driver
 driver.quit()
-
-
 
 
 if __name__ == '__main__':
