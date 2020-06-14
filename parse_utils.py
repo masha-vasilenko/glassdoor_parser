a1e3de282bd5ad8c6fd4b53115173764118b5a98
sergeibaranov <sergey.baranov.alt@gmail.com>
2020-06-14 16:07:51 -0700

    Move all browsing logic to main.py, add processing helper

--- parse_utils.py
+++ parse_utils.py
@@ -2,14 +2,14 @@ import logging
 import logging.config
 from lxml import etree
 import time
+import nltk
+import string
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('parser')
 
-# TODO: Add data processing helper
 
-
-def get_reviews(doc):
+def get_reviews(doc, company):
     results = []
     reviews = doc.cssselect("li.empReview.cf")
     get_role = etree.XPath(f".//span[{_klass('reviewer')}]/text()")
@@ -29,6 +29,7 @@ def get_reviews(doc):
             'application': (get_application(review) or [None])[0],
             'details': (get_interview(review) or [None])[0],
             'questions': get_questions(review),
+            'company': company,
         }
         results.append(data)
     return results
@@ -50,9 +51,10 @@ def get_next_page(doc):
 
 def gd_login(driver, login_url, email, pwd):
 
-    # Given the driver and credentials, login
     driver.get(login_url)
-    email_field = driver.find_element_by_xpath("//*[@type='submit']//preceding::input[2]")
+    email_field = driver.find_element_by_xpath(
+        "//*[@type='submit']//preceding::input[2]"
+    )
     email_field.send_keys(email)
     pwd_field = driver.find_element_by_xpath(
         "//*[@type='submit']//preceding::input[1]"
@@ -60,3 +62,61 @@ def gd_login(driver, login_url, email, pwd):
     pwd_field.send_keys(pwd)
     pwd_field.submit()
     time.sleep(3)
+
+
+def enter_company_name(element, company):
+    element.clear()
+    element.send_keys(company)
+    element.submit()
+
+
+def enter_location(element, location):
+    element.clear()
+    # element.send_keys(location)
+    # element.submit()
+
+
+def pick_company_from_search_results(doc, company):
+    get_links = etree.XPath(f"//a[contains(@href, 'Overview')]")
+    links = get_links(doc)
+    best_link = None
+    best_score = float('inf')
+    for link in links:
+        if link.text:
+            score = get_word_distance(company.lower(), link.text.lower().strip())
+            if score < best_score:
+                best_link = link.attrib['href']
+                best_score = score
+    if best_link:
+        return best_link.strip('/')
+
+
+def get_word_distance(target, word):
+    return nltk.edit_distance(target, word)
+
+
+def preprocess(reviews):
+    for review in reviews:
+        try:
+            review['helpful'] = int(''.join([c for c in review['helpful'] if c in string.digits]))
+        except TypeError:
+            review['helpful'] = None
+        except ValueError:
+            review['helpful'] = None
+
+        try:
+            review['accepted'] = review['outcomes'][0]
+            review['experience'] = review['outcomes'][1]
+            review['difficulty'] = review['outcomes'][2]
+        except IndexError:
+            review['accepted'] = ''
+            review['experience'] = ''
+            review['difficulty'] = ''
+
+        questions_pr = []
+        for question in review.get('questions', []):
+            question_pr = {
+                'question_text': question
+            }
+            questions_pr.append(question_pr)
+        review['questions'] = questions_pr
